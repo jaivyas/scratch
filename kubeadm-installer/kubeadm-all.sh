25cbc34e778a921bdd2f52f720aa32ec5ae20bc3
Rahul Nair <rhlnair87@gmail.com>
2018-04-28 11:29:10 +0530

    Fixed some silly mistakes  Please enter the commit message for your changes. Lines starting

--- kubeadm-installer/kubeadm-all.sh
+++ kubeadm-installer/kubeadm-all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-MASTER_IP="ec2-UB01"
-WORKER_IPs=(ec2-UB02)
+MASTER_IP="master01"
+WORKER_IPs=(worker01 worker02)
 INSTALLER_SCRIPT="kubeadm-installer.sh"
 MASTER_CONFIG_SCRIPT="kubeadm-master.sh"
-USER=ubuntu
+USER=admin
 
 echo -e "copying installer to $MASTER_IP"
 scp $INSTALLER_SCRIPT $USER@$MASTER_IP:.
@@ -22,10 +22,10 @@ done
 
 echo -e "Configuring master"
 
-ssh $MASTER_IP "sudo kubeadm init"
-ssh $MASTER_IP "mkdir -p ~/.kube;sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config;sudo chown $(id -u):$(id -g) ~/.kube/config;"
+ssh $USER@$MASTER_IP "sudo swapoff -a;sudo kubeadm init"
+ssh $USER@$MASTER_IP "mkdir -p ~/.kube;sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config;sudo chown $(id -u):$(id -g) ~/.kube/config;"
 
-CMD=$(ssh $MASTER_IP "sudo kubeadm token create --print-join-command")
+CMD=$(ssh $USER@$MASTER_IP "sudo kubeadm token create --print-join-command")
 
 echo -e "Master should be ready"
 
@@ -38,11 +38,11 @@ for i in ${WORKER_IPs[@]}; do
 done
 
 echo -e "Applying dashboard"
-scp dashboard-rbac.yml ingress.sh $MASTER_IP:.
-ssh $MASTER_IP "kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml; kubectl create -f dashboard-rbac.yml;kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}')"
+scp dashboard-rbac.yml ingress.sh $USER@$MASTER_IP:.
+ssh $USER@$MASTER_IP "kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml; kubectl create -f dashboard-rbac.yml;kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}')"
 
 echo -e "Use the above token to access kubernetes dashboard URL https://$MASTER_IP:6443/ui"
 
 echo -e "Applying trafiek ingress"
 
-ssh $MASTER_IP "bash ingress.sh"
+ssh $USER@$MASTER_IP "bash ingress.sh"
